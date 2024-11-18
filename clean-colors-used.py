a14de7f9b0b9283130e67424b74f6335726b4cc7
Abrielle <125398766+abrielleperry@users.noreply.github.com>
2024-11-18 12:01:06 -0600

    change painting title to title in colors used file

--- clean-colors-used.py
+++ clean-colors-used.py
@@ -1,17 +1,21 @@
 import pandas as pd
 
-data = pd.read_csv('Colors-Used.csv')
+data = pd.read_csv("Colors-Used.csv")
 
 # replace underscores with spaces and capitalize each word
-data.columns = [col.replace('_', ' ').title() for col in data.columns]
+data.columns = [col.replace("_", " ").title() for col in data.columns]
 
 # specific name changes
-data.rename(columns={
-    'Num Colors': 'Number of Colors',
-    'Youtube Src': 'Youtube Source',
-    'Unnamed: 0': 'Index',
-    'Img Src': 'Image Source'
-}, inplace=True)
+data.rename(
+    columns={
+        "Num Colors": "Number of Colors",
+        "Youtube Src": "Youtube Source",
+        "Unnamed: 0": "Index",
+        "Img Src": "Image Source",
+        "Painting Title": "Title",
+    },
+    inplace=True,
+)
 
 # save updated version
-data.to_csv('Colors-Used.csv', index=False)
+data.to_csv("Colors-Used.csv", index=False)
